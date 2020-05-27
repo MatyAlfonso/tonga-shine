f08d350306322697ec41cd2395e06153b1b96603
MatyAlfonso <matt1998boca@gmail.com>
2020-05-27 19:36:13 -0300

    Primer commit

---
+++ src/app.js
@@ -0,0 +1,56 @@
+// ************ Require's ************
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+var express = require('express');
+var logger = require('morgan');
+var path = require('path');
+
+
+
+// ************ express() - (don't touch) ************
+
+var app = express();
+
+
+// ************ Middlewares - (don't touch) ************
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser());
+
+// ************ Template Engine - (don't touch) ************
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+// ************ WRITE YOUR CODE FROM HERE ************
+// ************ Route System require and use() ************
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+// ************ DON'T TOUCH FROM HERE ************
+// catch 404 and forward to error handler
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// ************ exports app - dont'touch ************
+module.exports = app;
